e8bdb0677d497d5f668f515566d4da045fbb4ca4
DerVerruckte <michael.heinhold@gmail.com>
2021-08-10 10:25:36 -0600

    added individualized tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,25 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 //insert list item function for when button is clicked
-var createTaskHandler = function() {
+var createTaskHandler = function () {
     //prevent browser from refreshing
     event.preventDefault();
-    
-    //create the task item
-    var listItemEl =document.createElement("li");
-    listItemEl.className ="task-item";
-    listItemEl.textContent="This is a task item.";
+
+    //collect user data in input field
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    //create the list item
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+
+    //create div to hold info and add to list item
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info"
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" +taskNameInput + "</h3><span class='task-type'>"+ taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
     tasksToDoEl.appendChild(listItemEl);
 }
 
